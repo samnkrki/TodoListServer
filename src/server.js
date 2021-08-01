75cfba2dca66ae65b829f30ec813b61f58cc5c79
samnkrki <samn_krki@live.com>
2021-08-01 07:41:16 +0545

    babel updated, scripts fixed

--- src/server.js
+++ src/server.js
@@ -1,42 +1,40 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import bodyParser from 'body-parser'
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 
-import config from './config/config'
-import routes from './app/server.route'
+import config from "./config/config";
+import routes from "./app/server.route";
 
-const port = config.port
-const app = express()
+const port = config.port;
+const app = express();
 
-mongoose.connect(config.dbPath)
-var db = mongoose.connection
+// mongoose.connect(config.dbPath)
+// var db = mongoose.connection
 
-db.on('open', () => {
-    console.log('connected to the database......')
-})
+// db.on('open', () => {
+//     console.log('connected to the database......')
+// })
 
-db.on('error', (err) => {
-    console.log(err)
-})
+// db.on('error', (err) => {
+//     console.log(err)
+// })
 
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use('/api', routes)
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use("/api", routes);
 
-app.get('/', (req, res) => {
-    res.send('Todo-list Application')
-})
+app.get("/", (req, res) => {
+  res.send("Todo-list Application");
+});
 
 app.use((err, req, res, next) => {
-
-    res.status(err.status || 500).json({
-        msg: err.message,
-        stack: err.stack,
-    })
-})
+  res.status(err.status || 500).json({
+    msg: err.message,
+    stack: err.stack,
+  });
+});
 
 app.listen(port, () => {
-    console.log(`The todo-list server islistening on port ${port}`)
-})
+  console.log(`The todo-list server islistening on port ${port}`);
+});
